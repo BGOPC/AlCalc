ef382fc2ffe4aa93738515aeb65d7964b6358185
BGOPC <bb8933400@gmail.com>
2023-01-15 15:06:26 +0330

    complex multiplication

--- src/util/complex.java
+++ src/util/complex.java
@@ -1,41 +1,48 @@
 package src.util;
 
-public class complex{
+public class complex {
     public Integer real;
     public Power image;
-    public complex(Integer real, Power image){
+
+    public complex(Integer real, Power image) {
         this.real = real;
         this.image = image;
     }
-    public String toString(){
-        return this.real.toString() + this.image;
-    };
-    complex numMul(Integer LHS, complex RHS){
-        int compR = LHS * RHS.real;
 
+    public String toString() {
+        return this.real.toString() + this.image.toString();
+    };
+
+    complex numMul(Integer LHS, complex RHS) {
+        int compR = LHS * RHS.real;
         return new complex(compR, RHS.image);
     }
-    complex varMul(Power LHS, complex RHS){
+
+    complex varMul(Power LHS, complex RHS) {
         Power compL = LHS.mul(RHS.image);
-        return new complex(RHS.real, compL);
+        return new complex(this.real, compL);
     }
-    public void mul(complex RHS){ //complex
-        
+
+    public void mul(complex RHS) { // complex
+        System.out.println(this.numMul(this.real, RHS).varMul(this.image, RHS).toString());
     }
 }
 
-class Power{
-    public Integer base;
+class Power {
+    public String base;
     public Integer pow;
-    public Power(int base, int power){
+
+    public Power(String base, int power) {
         this.base = base;
         this.pow = power;
     }
-    public String toString(){
-        return this.base.toString() + "^" + this.pow.toString();
+
+    public String toString() {
+        return this.base + "^" + this.pow.toString();
     }
-    public Power mul(Power num) throws IllegalArgumentException{
-        if (this.base == num.base){
+
+    public Power mul(Power num) throws IllegalArgumentException {
+        if (this.base == num.base) {
             return new Power(num.base, num.pow + this.pow);
         }
         throw new IllegalArgumentException("different bases aren't supported");
