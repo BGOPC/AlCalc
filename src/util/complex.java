445330fc9f5f476bea18b57a1ebe789bafa6be26
BGOPC <bb8933400@gmail.com>
2023-01-15 15:45:08 +0330

    Div, Mul edit

--- src/util/complex.java
+++ src/util/complex.java
@@ -1,23 +1,30 @@
 package src.util;
 
 public class complex {
-    public Integer real;
+    public Float real;
     public Power image;
 
-    public complex(Integer real, Power image) {
+    public complex(Float real, Power image) {
         this.real = real;
         this.image = image;
     }
-    public complex(Power image) {
-        this.real = 1;
+
+    public complex(Integer real, Power image) {
+        this.real = real.floatValue();
         this.image = image;
     }
+
+    public complex(Power image) {
+        this.real = Integer.valueOf(1).floatValue();
+        this.image = image;
+    }
+
     public String toString() {
         return this.real.toString() + this.image.toString();
     };
 
-    complex numMul(Integer LHS, complex RHS) {
-        int compR = LHS * RHS.real;
+    complex numMul(Float LHS, complex RHS) {
+        Float compR = LHS * RHS.real;
         return new complex(compR, RHS.image);
     }
 
@@ -29,14 +36,30 @@ public class complex {
     public complex mul(complex RHS) {
         return this.numMul(this.real, RHS).varMul(this.image, RHS);
     }
+
+    complex numDiv(Float LHS, complex RHS) {
+        Float compR = LHS / RHS.real;
+        return new complex(compR, RHS.image);
+    }
+
+    complex varDiv(Power LHS, complex RHS) {
+        Power compL = LHS.div(RHS.image);
+        return new complex(this.real, compL);
+    }
+
+    public complex div(complex RHS) {
+        return this.numDiv(this.real, RHS).varDiv(this.image, RHS);
+    }
+
     public complex add(complex RHS) throws IllegalArgumentException {
-        if (this.image == RHS.image){
+        if (this.image == RHS.image) {
             return new complex(this.real + RHS.real, this.image);
         }
         throw new IllegalArgumentException("different images are not supported");
     }
+
     public complex sub(complex RHS) throws IllegalArgumentException {
-        if (this.image == RHS.image){
+        if (this.image == RHS.image) {
             return new complex(this.real - RHS.real, this.image);
         }
         throw new IllegalArgumentException("different images are not supported");
@@ -59,6 +82,14 @@ class Power {
     public Power mul(Power num) throws IllegalArgumentException {
         if (this.base == num.base) {
             return new Power(num.base, num.pow + this.pow);
+        } else if (this.pow == num.pow){
+            return new Power(this.base+num.base, this.pow);
+        }
+        throw new IllegalArgumentException("different bases aren't supported");
+    }
+    public Power div(Power num) throws IllegalArgumentException {
+        if (this.base == num.base) {
+            return new Power(num.base, num.pow - this.pow);
         }
         throw new IllegalArgumentException("different bases aren't supported");
     }
