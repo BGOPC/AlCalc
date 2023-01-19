256aec55bae21f54292e0ae238530c13aa439352
BGOPC <bb8933400@gmail.com>
2023-01-19 21:08:30 +0330

    update to calcuation

--- src/calculator.java
+++ src/calculator.java
@@ -1,5 +1,19 @@
 package src;
 
+import java.util.Scanner;
+import src.calc.Tokenizer;
+
 public class calculator {
-    
+    public static Scanner input = new Scanner(System.in);
+    public static String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String varName;
+    public static void main(String[] args) {
+        String eq = input.nextLine();
+        for (String letter : eq.split("")){
+            if (letters.contains(letter)){
+                varName = letter;
+                break;
+            }
+        }
+    }
 }
