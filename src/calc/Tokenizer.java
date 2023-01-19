ac2d95370515cb43d69e5ab92b0d182d8c769cfd
BGOPC <bb8933400@gmail.com>
2023-01-19 21:54:01 +0330

    Realizing my problems of Powers and complexes, Fixing Lv.1

--- src/calc/Tokenizer.java
+++ src/calc/Tokenizer.java
@@ -1,6 +1,5 @@
 package src.calc;
 
-
 import src.util.complex;
 import src.util.Power;
 import java.util.ArrayList;
@@ -9,10 +8,11 @@ import java.util.HashMap;
 public class Tokenizer {
     public String[] toks;
     public String varName;
-    public HashMap<complex,String> Output = new HashMap<complex,String>();
+    public HashMap<complex, String> Output = new HashMap<complex, String>();
     public ArrayList<complex> Complexes = new ArrayList<>();
     public ArrayList<String> Operators = new ArrayList<>();
     public static String ops = "+-*/";
+
     public Tokenizer(String toks, String varName) {
         this.toks = toks.split(" ");
         this.varName = varName;
@@ -22,13 +22,31 @@ public class Tokenizer {
         return String.join(" ", this.toks);
     }
 
-    public HashMap<complex,String> Tokenize() {
+    public String calcNumPow(String eq) {
+        if (!eq.contains("^")) {
+            return eq;
+        } else {
+            Integer num = Integer.valueOf(eq.split("\\^")[0]);
+            Integer pow = Integer.valueOf(eq.split("\\^")[1]);
+            return String.valueOf(Math.pow(num, pow));
+        }
+    }
+
+    public HashMap<complex, String> Tokenize() {
         for (String tok : this.toks) {
-            if (!ops.contains(tok)){
-                Float real = Float.parseFloat(tok.split(varName)[0]);
-                String strImage = "x"+tok.split(varName)[1];
-                Power image = new Power(varName,Integer.parseInt(strImage.split("^")[0]));
-                this.Complexes.add(new complex(real,image));
+            if (!ops.contains(tok)) {
+                Float real;
+                String strImage = "";
+                Power image;
+                if (tok.contains(varName)) {
+                    real = Float.valueOf(calcNumPow(tok.split(this.varName)[0]));
+                    strImage = "x" + tok.split(this.varName)[1];
+                    image = new Power(varName, Integer.parseInt(strImage.split("^")[0]));
+                } else {
+                    real = Float.valueOf(calcNumPow(tok));
+                    image = new Power(varName, 0);
+                }
+                this.Complexes.add(new complex(real, image));
             } else {
                 this.Operators.add(tok);
             }
@@ -36,7 +54,7 @@ public class Tokenizer {
         for (int i = 0; i < this.Complexes.size(); i++) {
             String op = "";
             complex Complex = this.Complexes.get(i);
-            if (i < this.Operators.size()){
+            if (i < this.Operators.size()) {
                 op = this.Operators.get(i);
             }
             Output.put(Complex, op);
--- src/calculator.java
+++ src/calculator.java
@@ -1,12 +1,16 @@
 package src;
 
 import java.util.Scanner;
+import java.util.HashMap;
 import src.calc.Tokenizer;
+import src.util.*;
 
 public class calculator {
     public static Scanner input = new Scanner(System.in);
     public static String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static String varName;
+    public static HashMap<complex,String> Tokenized = new HashMap<complex,String>();
+
     public static void main(String[] args) {
         String eq = input.nextLine();
         for (String letter : eq.split("")){
@@ -15,5 +19,8 @@ public class calculator {
                 break;
             }
         }
+        Tokenizer Tokens = new Tokenizer(eq, varName);
+        Tokenized = Tokens.Tokenize();
+        // System.out.println(Tokenized);
     }
 }
